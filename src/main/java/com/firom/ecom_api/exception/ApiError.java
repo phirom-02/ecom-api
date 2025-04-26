5d5759c97fb76e4b1828750bcd9a4182d8c9c25b
phirom-02 <phiromkhim02@gmail.com>
2025-04-26 15:23:54 +0700

    feat[ApiError]: add extra properties

--- src/main/java/com/firom/ecom_api/exception/ApiError.java
+++ src/main/java/com/firom/ecom_api/exception/ApiError.java
@@ -1,5 +1,7 @@
 package com.firom.ecom_api.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.firom.ecom_api.exception.enums.ErrorCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
@@ -9,19 +11,41 @@ import java.util.Arrays;
 
 @Setter
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
     private String errorCode;
     private String error;
     private String message;
     private String trace;
+    private String description;
     private Instant timestamp;
 
-//    Todo: Add custom errorCode
-    public ApiError(HttpStatus httpStatus, String message, StackTraceElement[] trace) {
+    public  ApiError() {}
+
+    public  ApiError(
+            HttpStatus httpStatus,
+            ErrorCode errorCode,
+            StackTraceElement[] trace
+    ) {
         this.error = httpStatus.getReasonPhrase();
-        this.message = message;
         this.trace = Arrays.toString(trace);
         this.timestamp = Instant.now();
-        this.errorCode = "1000";
+        this.message = errorCode.getMessage();
+        this.errorCode = errorCode.getCode();
+        this.description = null;
+    }
+
+    public ApiError(
+            HttpStatus httpStatus,
+            ErrorCode errorCode,
+            StackTraceElement[] trace,
+            String description
+    ) {
+        this.error = httpStatus.getReasonPhrase();
+        this.trace = Arrays.toString(trace);
+        this.timestamp = Instant.now();
+        this.message = errorCode.getMessage();
+        this.errorCode = errorCode.getCode();
+        this.description = description;
     }
 }
