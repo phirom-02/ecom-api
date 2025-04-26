b7fa7c9e2213879b4005afa67ce53abf85df4257
phirom-02 <phiromkhim02@gmail.com>
2025-04-26 15:22:03 +0700

    feat: create an ErrorCode enum class

---
+++ src/main/java/com/firom/ecom_api/exception/enums/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.firom.ecom_api.exception.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // General error codes: 1000 - 1999
+    INTERNAL_SERVER_ERROR("1000", "Internal Server Error"),
+    BAD_REQUEST("1001", "Bad Request"),
+    FORBIDDEN("1002", "Forbidden"),
+    UNAUTHORIZED("1003", "Unauthorized"),
+    VALIDATION_ERROR("1004", "Validation Error"),
+
+    // Auth error codes: 2000 - 2999
+
+    // Product error codes: 3000 - 3999
+    PRODUCT_NOT_FOUND("3000", "Product not found");
+
+
+    private final String code;
+    private final String message;
+}
