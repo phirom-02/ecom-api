6310ed1eacd884b48ef2ad82a57ef1a6d59f7cfb
phirom-02 <phiromkhim02@gmail.com>
2025-05-02 23:03:15 +0700

    feat: add a new InvalidTokenException class

---
+++ src/main/java/com/firom/ecom_api/common/exceptions/InvalidTokenException.java
@@ -0,0 +1,14 @@
+package com.firom.ecom_api.common.exceptions;
+
+import com.firom.ecom_api.common.enums.ErrorCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InvalidTokenException extends CustomRuntimeException {
+
+    public InvalidTokenException() {
+        super("Invalid token", ErrorCode.UNAUTHORIZED);
+    }
+}
